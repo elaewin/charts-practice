5d3e5eb62c9c892f64cd9d721ea94e066ef958dc
Erica Winberry <elaewin@gmail.com>
2017-04-13 15:38:57 -0700

    Charts importing, but not showing the chart views in storyboard. Possibly something that needs to be fixed to update Charts to latest version of Swift/Xcode.

---
+++ ChartsInSwift/TestLineChartView.swift
@@ -0,0 +1,22 @@
+//
+//  TestLineChartView.swift
+//  ChartsInSwift
+//
+//  Created by Erica Winberry on 3/13/17.
+//  Copyright © 2017 Erica Winberry. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class TestLineChartView: LineChartView {
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
